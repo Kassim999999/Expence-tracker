662a1d7a996b927ca0bd7883d58710c9fb3acae7
Kassim <kassimrooney@gmail.com>
2025-04-14 23:53:42 +0300

    Removed value attribute

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Searchbar = () => {
   return (
     <div>
-        <input type="text" placeholder='Search' value={searchTerm} />
+        <input type="text" placeholder='Search'/>
     </div>
   )
 }
