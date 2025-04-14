5761d558630c317478958847ec7d30c8f2756f18
Kassim <kassimrooney@gmail.com>
2025-04-14 23:52:02 +0300

    Imported Searchbar Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Form from './components/Form';
 import Header from './components/Header';
+import Searchbar from './components/Searchbar';
 
 function App() {
   return (
     <div>
       <Header/>
       <Form/>
+      <Searchbar/>
     </div>
   
   );
