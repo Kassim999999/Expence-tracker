fe02b5f5c3908af89bdf5b57db8e33fcafee79a3
Kassim <kassimrooney@gmail.com>
2025-04-14 23:37:23 +0300

    Created Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>Expense Tracker</h1>
+        <h3>Start taking control of your finances and life</h3>
+    </div>
+  )
+}
+
+export default Header
