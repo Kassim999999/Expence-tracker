066dd68031f4d90b8ed996c7350b92046f402068
Kassim <kassimrooney@gmail.com>
2025-04-15 00:53:43 +0300

    Styled Form Component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>
+    <div id='form-container'>
         <h2>Add Expense</h2>
-        <form>
+        <form className='form'>
             <input type="text" placeholder='Expense Name' />
             <input type="text" placeholder='Description'/>
             <input type="text" placeholder='Category'/>
             <input type="number" placeholder='Amount'/>
             <input type="text" />
-            <button type='submit'>Submit</button>
+            <button type='submit' className='btn'>Submit</button>
         </form>
     </div>
   )
