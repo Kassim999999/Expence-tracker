1125b467a507f5b288249552a3976cb168dcd04d
Kassim <kassimrooney@gmail.com>
2025-04-15 11:24:20 +0300

    Added attriibutes in Searchbar Component

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const Searchbar = () => {
+const Searchbar = ({searchExpense}) => {
   return (
     <div id='finder'>
         <i className="fa-solid fa-magnifying-glass"></i>
-        <input type="text" placeholder='Search'/>
+        <input type="text" 
+        placeholder='Search'
+        value={searchExpense}
+        onChange={(event)=> searchExpense(event.target.value)}
+        
+        />
     </div>
   )
 }
